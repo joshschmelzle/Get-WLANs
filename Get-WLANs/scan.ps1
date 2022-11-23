a64af1ea7cd0a1f738fee0978159ba7b31348a22
Josh Schmelzle <josh@joshschmelzle.com>
2022-11-22 21:23:00 -0500

    add import-module example for local module

--- Get-WLANs/scan.ps1
+++ Get-WLANs/scan.ps1
@@ -1,6 +1,9 @@
 # assuming module is installed from PSGallery
 Import-Module Get-WLANs
 
+# assuming module is local
+# Import-Module .\Get-WLANs
+
 # run scan, sort by RSSI, and display as a Format-Table
 Get-WLANs | Sort-Object -Property RSSI -Descending | Format-Table
 
