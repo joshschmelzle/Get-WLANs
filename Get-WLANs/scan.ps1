c78e5d2e8292fcebf1e6aad7586f9e3970a32bea
Josh Schmelzle <josh@joshschmelzle.com>
2022-11-30 23:25:11 -0500

    use try finally to remove module in error scenario

--- Get-WLANs/scan.ps1
+++ Get-WLANs/scan.ps1
@@ -1,14 +1,18 @@
-# assuming module is installed from PSGallery
-# Import-Module Get-WLANs
+try {   
+    # assuming module is installed from PSGallery
+    # Import-Module Get-WLANs
 
-# assuming module is local
-Import-Module .\Get-WLANs
+    # assuming module is local
+    Import-Module .\Get-WLANs
 
-# run scan, sort by RSSI, and display as a Format-Table
-Get-WLANs | Sort-Object -Property RSSI -Descending | Format-Table
+    # run scan, sort by RSSI, and display as a Format-Table
+    # Get-WLANs -Verbose
+    Get-WLANs -Verbose | Sort-Object -Property RSSI -Descending | Format-Table
 
-# export to CSV example
-# Get-WLANs | Select-Object * | Export-Csv -Path .\Get-WLANs.csv -NoTypeInformation
-
-# remove module after we're done with it
-Remove-Module Get-WLANs
+    # export to CSV example
+    # Get-WLANs | Select-Object * | Export-Csv -Path .\Get-WLANs.csv -NoTypeInformation
+}
+finally {
+    # remove module after we're done with it
+    Remove-Module Get-WLANs
+}
